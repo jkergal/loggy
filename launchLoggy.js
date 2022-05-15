469c21d3e99734a4a7db19c7064d3c2e6995c5f6
Johann Kergal <hello@johannkergal.fr>
2022-05-16 00:04:30 +0200

    clean (launchLoggy) : change env vars names

--- launchLoggy.js
+++ launchLoggy.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 module.exports = async function launchLoggy () {
     console.log("Loggy is launching")
     const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
-    await client.login(process.env.DISCORD_TOKEN)
+    await client.login(process.env.DISCORD_LOGGY_TOKEN)
     await client.once('ready', () => {
         console.log('READY TO DISPLAY LOGS !')
     })
