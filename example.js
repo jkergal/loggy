b1c456d0119210de8b15f86175637c52cd994da6
Johann Kergal <hello@johannkergal.fr>
2022-05-17 11:03:50 +0200

    feat (loggy) : WIP try to get channel var out of the sending message method to be able to not have the client arg il the log() method

--- example.js
+++ example.js
@@ -1,18 +1,16 @@
 const loggy = require('./loggy/loggy')
-const loggyClient = require('./loggy/loggyClient')
-const quitLoggy = require('./loggy/quitLoggy')
-// const loggyNotification = require('./loggy/loggyNotification')
 
 const startLoggyClient = async () => {
-  const client = await loggyClient()
+  const client = await loggy.client()
   return client
 }
 
 startLoggyClient()
   .then((client) => {
     // DO SOME STUFF AND LOG IT ON DISCORD WITH logg(client, yourLogMessage)
-    loggy.log(client, "PROUT")
+    // console.log(client)
+    loggy.log('PROUT')
     // AND QUIT LOGGY IF YOU NEED IT
-    quitLoggy(client)
+    // loggy.quit(client)
   })
 
--- loggy/loggy.js
+++ loggy/loggy.js
@@ -1,30 +1,76 @@
 require('discord.js');
+const { Client, Intents } = require('discord.js');
+require('dotenv').config()
+
+let channel 
 
 class Loggy {
-        async log(client, message) {
+    async client() {
+        const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
 
-            const logChannel = await client.channels.cache.get(process.env.CHANNEL_ID_LOGS)
-            logChannel.send(message)
+        console.log("Loggy is launching")
+                
+        client.once('ready', () => {
+            console.log('READY TO DISPLAY LOGS !')
+        })
+
+        client.login(process.env.DISCORD_LOGGY_TOKEN)
+
+        channel = client.channels.cache.get((process.env.CHANNEL_ID_LOGS))
         
+        // function getChannel() {
+        //     console.log(client.isReady())
+        //     if(client.isReady() === false) {
+        //         console.log("PROUT")
+        //        setTimeout(getChannel, 1000); /* this checks the flag every 100 milliseconds*/
+        //     } else {
+        //         console.log(client.isReady())
+        //         console.log('getting channel')
+        //         channel = client.channels.cache.get((process.env.CHANNEL_ID_LOGS))
+        //         return
+        //     }
+        // }
+
+        // getChannel()
+
+        return client
+    }
+
+        async log(message) {
+
+            await channel
+            channel.send(message)
+            // console.log(channelID)
+
         }
         
-        async alert(client, message) {
+        // async alert(client, message) {
         
-            const userId = process.env.USER_ID
-            const cli = await client
-            const logChannel = await cli.channels.cache.get(process.env.CHANNEL_ID_NOTIFICATIONS)
-            await logChannel.send(`${message} --- <@${userId.toString()}>`)
+        //     const userId = process.env.USER_ID
+        //     const logChannel = await client.channels.cache.get(process.env.CHANNEL_ID_NOTIFICATIONS)
+        //     logChannel.send(`${message} --- <@${userId.toString()}>`)
         
-        }    
+        // }    
 
-        async error(client, message) {
+        // async error(client, message) {
         
-            const userId = process.env.USER_ID
-            const cli = await client
-            const logChannel = await cli.channels.cache.get(process.env.CHANNEL_ID_NOTIFICATIONS)
-            await logChannel.send(`${message} --- <@${userId.toString()}>`)
+        //     const userId = process.env.USER_ID
+        //     const cli = await client
+        //     const logChannel = await cli.channels.cache.get(process.env.CHANNEL_ID_NOTIFICATIONS)
+        //     await logChannel.send(`${message} --- <@${userId.toString()}>`)
         
-        }  
+        // }
+
+        quit(client) {
+
+            console.log("Waiting for Loggy's deconnexion : 10sec...")
+            
+            setTimeout(
+                async function destroy() {
+                await client.destroy()
+            }, 10000)
+        
+        }
 }
 
 const loggy = new Loggy()
