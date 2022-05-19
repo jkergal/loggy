096078a55ab54d4371f9dce35282ccc65efb634c
YoannBDL <yoannb64@gmail.com>
2022-05-19 21:13:50 +0200

    It's working

--- loggy/loggy.js
+++ loggy/loggy.js
@@ -1,112 +1,37 @@
-require('discord.js');
-const { Client, Intents, DataResolver } = require('discord.js');
-require('dotenv').config()
+require("discord.js");
+const { Client, Intents, DataResolver } = require("discord.js");
+require("dotenv").config();
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
-
-// let channel
-
-const getChannel = () => {
-    return new Promise((resolve) => {
-        if(client.isReady() === true) {
-            console.log(client.isReady())
-            console.log('getting channel')
-            let channel = client.channels.cache.get(process.env.CHANNEL_ID_LOGS)
-            resolve(channel)
-        } else {
-            console.log(client.isReady())
-            setTimeout(() => {getChannel()}, 1000)
-        }
-    })
-}
 class Loggy {
-    async client() {
-        
+  discordJsClient;
 
-        console.log("Loggy is launching")
-                
-        client.once('ready', () => {
-            console.log('READY TO DISPLAY LOGS !')
-            
-        })
+  constructor() {
+    this.discordJsClient = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
+  }
 
-        client.login(process.env.DISCORD_LOGGY_TOKEN)
+  async client() {
+    console.log("Loggy is launching");
 
-        // await isChannelReady().then((isChannelReady) => {
-        //     console.log('le channel est ready :')
-        //     return isChannelReady()
-        // })
+    this.discordJsClient.once("ready", () => {
+      console.log("READY TO DISPLAY LOGS !");
+    });
 
-        // console.log(channelReady)
+    await this.discordJsClient.login(process.env.DISCORD_LOGGY_TOKEN);
+  }
 
-        return client
+  async log(message) {
+    await this.discordJsClient.channels.cache.get(process.env.CHANNEL_ID_LOGS).send(message);
+  }
 
-    }
+  quit(client) {
+    console.log("Waiting for Loggy's deconnexion : 10sec...");
 
-    // channelReady = async () => {
-
-    //     let channelReady
-    
-    //     console.log("POMME")
-        
-    //     try {
-    //         channelReady = await loggy.getChannel()
-    //         console.log('fraise')
-    //     } catch {
-    //         console.error('error')
-    //     }
-
-    //     return channelReady
-        
-    // }
-
-    async log(message) {
-
-        let channelReady
-        
-        try {
-            channelReady = await getChannel()
-        } catch {
-            console.error('error')
-        }
-            channelReady.send(message)
-
-
-        // console.log(channelID)
-
-    }
-    
-    // async alert(client, message) {
-    
-    //     const userId = process.env.USER_ID
-    //     const logChannel = await client.channels.cache.get(process.env.CHANNEL_ID_NOTIFICATIONS)
-    //     logChannel.send(`${message} --- <@${userId.toString()}>`)
-    
-    // }    
-
-    // async error(client, message) {
-    
-    //     const userId = process.env.USER_ID
-    //     const cli = await client
-    //     const logChannel = await cli.channels.cache.get(process.env.CHANNEL_ID_NOTIFICATIONS)
-    //     await logChannel.send(`${message} --- <@${userId.toString()}>`)
-    
-    // }
-
-    quit(client) {
-
-        console.log("Waiting for Loggy's deconnexion : 10sec...")
-        
-        setTimeout(
-            async function destroy() {
-            await client.destroy()
-        }, 10000)
-    
-    }
+    setTimeout(async function destroy() {
+      await client.destroy();
+    }, 10000);
+  }
 }
 
-const loggy = new Loggy()
+const loggy = new Loggy();
 
-module.exports = loggy
-
-
+module.exports = loggy;
